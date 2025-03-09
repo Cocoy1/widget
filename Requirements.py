755dc71d8f22f98fa4b470bde92189da26729fac
Santiago <136390230+Cocoy1@users.noreply.github.com>
2025-03-09 02:39:58 -0600

    Delete Requirements.py

--- Requirements.py
+++
@@ -1,7 +0,0 @@
-# Importar funciones de las cajas de información y barra de progreso 
-
-from widget.gradient_box import display_inside_box
-from widget.gradient_box import display_outside_box
-from widget.gradient_box import display_circular_box
-from widget.gradient_box import display_interactive_box
-from widget.gradient_bar import ProgressBarAnimated
